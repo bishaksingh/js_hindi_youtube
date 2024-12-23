e30a35966d7bf0038120d20f585964d2344b7b2d
Bishak singh <148785272+bishaksingh@users.noreply.github.com>
2024-12-23 17:14:53 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "Bishaksingh@gmail.com"
+var accountPass = "12345"
+accountCity = "West Bengal"
+
+// accountId = 2  // not allowed
+
+accountEmail = "Bishak@gmail.com"
+accountPass = "212121"
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity])
